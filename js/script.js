d43b82bf7447894d66051d771878da03fd72a6ef
nori0507 <chikyu.rei@ic.edu>
2025-10-29 21:09:08 -0500

    redoing step 7 bc I was not actually assigning the URL into images and I kept getting error in step 8

--- js/script.js
+++ js/script.js
@@ -2,20 +2,26 @@ console.log("script.js loaded");
 
 const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=V83DiBIHOu3SR4NMOo2N38wCXAiebbTH&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
 
+let images = [];
 // building a function to get GIF information
 async function getGIF() {
-// requesting data
-const response = await fetch(endpoint);
-// converting data to json
-const data = await response.json();
-// seeing what we get from the converted data
-console.log(data);
-// I found out the data was separated into few parts such as data, meta, pagnation
-// since I wanted to store unique URLs to the array images, I assinged data.data (data was the array with 25 unique URLs) into images
-// since data.data is array, image is array
-const images = data.data
-// cheking if imge is the array with unique URLs
-console.log(images)
+    // requesting data
+    const response = await fetch(endpoint);
+    // converting data to json
+    const converted = await response.json();
+    // seeing what we get from the converted data
+    console.log(converted);
+    // assigning unique URLs into images array
+    // From the API responce, we know the path for the URL
+    // converted -> data -> images -> original -> url
+    // .map() function iterate every GIF and the URL will be stored into the images array
+    images = converted.data.map(gif => gif.images.original.url);
+    // cheking if imge is the array with unique URLs
+    console.log(images)
 
 }
 getGIF()
+
+ 
+
+
